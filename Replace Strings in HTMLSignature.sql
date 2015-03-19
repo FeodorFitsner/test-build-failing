2ee47040c85cf72e96e749c2b7923a4fb327966c
Ricardo Polo <ricardo.polo@tulpep.com>
2015-03-19 18:59:07 -0500

    Removed files

--- Replace Strings in HTMLSignature.sql
+++
@@ -1,9 +0,0 @@
-UPDATE [dbo].[Signatures]
-   SET [HtmlString] = replace([HtmlString], '{CompanyString}', '{CompanyName}')
-GO
-
-UPDATE [dbo].[Signatures]
-   SET [HtmlString] = replace([HtmlString], '{Ext}', '{PhoneExtension}')
-GO
-
-
