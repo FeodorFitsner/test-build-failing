d6c36ac3ceebe5b874b66486b498b030b80d13d4
Ricardo Polo <ricardo.polo@tulpep.com>
2015-03-19 16:39:17 -0500

    First commit

---
+++ gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+    "bower-install-simple": {
+          options: {
+              color: true,
+              directory: "bower_components"
+          },
+          "prod": {
+              options: {
+                  production: true
+              }
+          }
+      },
+  });
+
+  grunt.loadNpmTasks('grunt-bower-install-simple');
+  grunt.registerTask('default', ['bower-install-simple']);
+
+};
